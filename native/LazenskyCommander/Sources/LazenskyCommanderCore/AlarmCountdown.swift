47475bd62a9c8bc4413b9318c0ebaa0a15d0efb9
VarnaKonvice <pet.bohanes@gmail.com>
2026-08-24 19:43:24 +0200

    fix AlarmKit countdown target planning

--- native/LazenskyCommander/Sources/LazenskyCommanderCore/AlarmCountdown.swift
+++ native/LazenskyCommander/Sources/LazenskyCommanderCore/AlarmCountdown.swift
@@ -1,5 +1,15 @@
 import Foundation
 
+public struct AlarmCountdownPlan: Equatable, Sendable {
+  public let scheduledStartAt: Date?
+  public let duration: TimeInterval
+
+  public init(scheduledStartAt: Date?, duration: TimeInterval) {
+    self.scheduledStartAt = scheduledStartAt
+    self.duration = duration
+  }
+}
+
 public enum AlarmCountdown {
   public static func preAlertDuration(for alarm: NativeAlarm, in schedule: Schedule) throws -> TimeInterval {
     let leaveAt = try NativeAlarmContract.date(fromLocalISO: alarm.leaveAt)
@@ -9,6 +19,21 @@ public enum AlarmCountdown {
       return endAt < leaveAt ? endAt : nil
     }.max()
     if let endAt = sameDayPreviousEnd { return max(0, leaveAt.timeIntervalSince(endAt)) }
-    return max(0, min(30 * 60, leaveAt.timeIntervalSince(Date(timeIntervalSince1970: leaveAt.timeIntervalSince1970 - 30 * 60))))
+    return 30 * 60
+  }
+
+  public static func plan(for alarm: NativeAlarm, in schedule: Schedule, now: Date) throws -> AlarmCountdownPlan {
+    let leaveAt = try NativeAlarmContract.date(fromLocalISO: alarm.leaveAt)
+    let desiredDuration = try preAlertDuration(for: alarm, in: schedule)
+    let desiredStartAt = leaveAt.addingTimeInterval(-desiredDuration)
+
+    if desiredStartAt > now {
+      return AlarmCountdownPlan(scheduledStartAt: desiredStartAt, duration: desiredDuration)
+    }
+
+    return AlarmCountdownPlan(
+      scheduledStartAt: nil,
+      duration: max(0, leaveAt.timeIntervalSince(now))
+    )
   }
 }
